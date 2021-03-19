b34c7ed23fd9744d4a15ad26fc554dc2207a3e1e
Veronika Balakhovsky <vbalakhovsky32@gmail.com>
2021-03-19 16:08:24 -0400

    added IndexedDB functions

---
+++ public/db.js
@@ -0,0 +1,66 @@
+const pendingObjectStoreName = 'pending';
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = event => {
+
+    const db = request.result;
+
+if(!db.objectStoreNames.contains(pendingObjectStoreName)){
+db.createObjectStore(pendingObjectStoreName, {autoIncrement: true});
+   }
+
+};
+
+request.onsuccess = event => {
+console.log("Success: ${event.type}");
+
+if(navigator.onLine) {checkDatabase();}
+
+};
+
+request.onerror = event => console.error(event);
+
+function checkDatabase(){
+const db = request.result;
+
+let transaction = db.transaction([pendingObjectStoreName], "readwrite");
+
+let store = transaction.objectStore(pendingObjectStoreName);
+
+const getAll = store.getAll();
+
+getAll.onsuccess = () => {
+
+if(getAll.result.length >0) {
+fetch("api/transaction/bulk" , {
+method: "POST",
+body: JSON.stringify(getAll.result),
+headers: {
+Accept: "application/json, text/plain, */*", 
+"Content-Type": "application/json"
+
+}
+
+
+})
+
+.then(response => response.json())
+.then(()=>{
+transaction = db.transaction([pendingObjectStoreName], "readwrite");
+
+store = transaction.objectStore(pendingObjectStoreName);
+
+store.clear();
+
+
+})
+
+}
+
+
+}
+
+
+
+
+}
